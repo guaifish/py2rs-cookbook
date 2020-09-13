1e2a0a75c0cc31c9eb4c2934770b4c0ed253bf4c
guaifish <guaifish@hotmail.com>
2020-09-14 00:20:20 +0800

    1.2 解压可迭代对象赋值给多个变量

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,5 @@
 fn main() {
-    let p = (4, 5);
-    let (x, y) = p;
-    assert_eq!(x, 4);
-    assert_eq!(y, 5);
-
-    let data = ("ACME", 50, 91.1, [2012, 12, 21]);
-    let (name, shares, price, date) = data;
-    assert_eq!(name, "ACME");
-    assert_eq!(shares, 50);
-    assert_eq!(price, 91.1);
-    assert_eq!(date, [2012, 12, 21]);
-
-    let s = "Hello";
-    let s: Vec<_> = s.chars().collect();
-    if let [a, b, c, d, e] = s[..] {
-        assert_eq!(a, 'H');
-        assert_eq!(b, 'e');
-        assert_eq!(c, 'l');
-        assert_eq!(d, 'l');
-        assert_eq!(e, 'o');
-    };
-
-    let data = ("ACME", 50, 91.1, [2012, 12, 21]);
-    let (_, shares, price, _) = data;
-    assert_eq!(shares, 50);
-    assert_eq!(price, 91.1);
+    let record = ("Dave", "dave@example.com", "773-555-1212", "847-555-1212");
+    // 好像无法匹配多个变量
+    // let (name, email, ..phone_numbers) = record;
 }
