289b84f1fa119d5b745c0471d199af15bb32862f
guaifish <guaifish@hotmail.com>
2020-09-13 20:15:43 +0800

    1.1 将序列分解为单独的变量

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let p = (4, 5);
+    let (x, y) = p;
+    assert_eq!(x, 4);
+    assert_eq!(y, 5);
+
+    let data = ("ACME", 50, 91.1, [2012, 12, 21]);
+    let (name, shares, price, date) = data;
+    assert_eq!(name, "ACME");
+    assert_eq!(shares, 50);
+    assert_eq!(price, 91.1);
+    assert_eq!(date, [2012, 12, 21]);
+
+    let s = "Hello";
+    let s: Vec<_> = s.chars().collect();
+    if let [a, b, c, d, e] = s[..] {
+        assert_eq!(a, 'H');
+        assert_eq!(b, 'e');
+        assert_eq!(c, 'l');
+        assert_eq!(d, 'l');
+        assert_eq!(e, 'o');
+    };
+
+    let data = ("ACME", 50, 91.1, [2012, 12, 21]);
+    let (_, shares, price, _) = data;
+    assert_eq!(shares, 50);
+    assert_eq!(price, 91.1);
 }
