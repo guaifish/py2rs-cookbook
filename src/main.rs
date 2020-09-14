27a85ca8346a6dc1c7e868a7fae9004d392574ce
guaifish <guaifish@hotmail.com>
2020-09-14 15:47:37 +0800

    1.7 字典排序

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-#[macro_use] extern crate maplit;
-use defaultmap::DefaultHashMap;
-use std::collections::HashSet;
-
+use linked_hash_map::LinkedHashMap;
 
 fn main() {
-    let mut m = DefaultHashMap::new(vec![]);
-    m['a'].push(1);
-    m['a'].push(2);
-    m['b'].push(4);
-    assert_eq!(m['a'], vec![1, 2]);
-    assert_eq!(m['b'], vec![4]);
-    assert_eq!(m['c'], vec![]);
-
-    let mut m = DefaultHashMap::new(HashSet::new());
-    m['a'].insert(1);
-    m['a'].insert(2);
-    m['b'].insert(4);
-    assert_eq!(m['a'], hashset!{1, 2});
-    assert_eq!(m['b'], hashset!{4});
-    assert_eq!(m['c'], HashSet::new());
+    let mut m = LinkedHashMap::new();
+    m.insert("foo", 1);
+    m.insert("bar", 2);
+    m.insert("spam", 3);
+    m.insert("grok", 4);
+    for (key, value) in m.iter() {
+        println!("{} {}", key, value);
+    }
 }
