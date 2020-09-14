acfc6a9fcb54426421316f25b21093841fbc98aa
guaifish <guaifish@hotmail.com>
2020-09-14 23:55:13 +0800

    1.18 映射名称到序列元素

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
-#[macro_use]
-extern crate maplit;
+struct Subscriber<'a> {
+    addr: &'a str,
+    joined: &'a str,
+}
 
-#[macro_use(c)]
-extern crate cute;
-
-use std::collections::HashMap;
+impl<'a> Subscriber<'a> {
+    fn new(addr: &'a str, joined: &'a str) -> Subscriber<'a> {
+        Subscriber { addr, joined }
+    }
+}
 
 fn main() {
-    let prices = hashmap! {
-        "ACME" => 45.23,
-        "AAPL" => 612.78,
-        "IBM" => 205.55,
-        "HPQ" => 37.20,
-        "FB" => 10.75,
-    };
-    
-    // py
-    let p1 = c! {k => v, for (k, v) in &prices, if *v > 200.0};
-    println!("{:?}", p1);
-    // rs
-    let p1: HashMap<_, _> = prices.iter().filter(|(_, v)| **v > 200.0).collect();
-    println!("{:?}", p1);
-
-    // py
-    let tech_names = hashset! {"AAPL", "IBM", "HPQ", "MSFT"};
-    let p2 = c! {k => v, for (k, v) in &prices, if tech_names.contains(k)};
-    println!("{:?}", p2);
-    // rs
-    let p2: HashMap<_, _> = prices
-        .iter()
-        .filter(|(k, _)| tech_names.contains(**k))
-        .collect();
-    println!("{:?}", p2);
+    let sub = Subscriber::new("jonesy@example.com", "2012-10-19");
+    println!("{:?}", sub.addr);
+    println!("{:?}", sub.joined);
 }
