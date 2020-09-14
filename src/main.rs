d256a094b3c6ae949b2453f13fdfc92f105802fe
guaifish <guaifish@hotmail.com>
2020-09-14 14:21:22 +0800

    1.4 查找最大或最小的 N 个元素

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
-use std::collections::VecDeque;
+use std::collections::BinaryHeap;
+
+trait Largest<T> {
+    fn nlargest(&self, n: u32) -> Vec<T>;
+}
+
+impl<T: Ord + Copy> Largest<T> for BinaryHeap<T> {
+    fn nlargest(&self, n: u32) -> Vec<T> {
+        let mut heaq = self.clone();
+        let mut v: Vec<T> = Vec::new();
+        if n > self.len() as u32 {
+            panic!("n is bigger than heaq's lengh")
+        }
+        for _ in 0..n {
+            if let Some(x) = heaq.pop() {
+                v.push(x)
+            } else {
+                unimplemented!()
+            }
+        }
+        v
+    }
+}
 
 fn main() {
-    let mut q = VecDeque::with_capacity(3);
-    q.push_back(1);
-    q.push_back(2);
-    q.push_back(3);
-    q.push_front(4);
-    assert_eq!(q, VecDeque::from(vec![4, 1, 2, 3])); // 没有定长
+    let nums = vec![1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2];
+    // 最大堆
+    let heap = BinaryHeap::from(nums);
+    println!("{:?}", heap.nlargest(3));
 }
