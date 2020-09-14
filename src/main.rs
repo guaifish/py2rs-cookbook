e672c0c6cd8c22432d3fb6be04d7d41c2eb2eeb8
guaifish <guaifish@hotmail.com>
2020-09-14 20:00:58 +0800

    1.11 命名切片

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,7 @@
-use std::collections::HashSet;
-
 fn main() {
-    let a = [1, 5, 2, 1, 9, 1, 5, 10];
-    let mut set = HashSet::new();
-    for i in a.iter() {
-        set.insert(i);
-    }
-    println!("{:?}", set);
-    let a = dedupe(&a);
-    println!("{:?}", a);
-}
-
-fn dedupe(items: &[i32]) -> Vec<i32> {
-    let mut seen = HashSet::new();
-    let mut v = Vec::new();
-    for item in items {
-        if !seen.contains(item) {
-            seen.insert(*item);
-            v.push(*item);
-        }
-    }
-    v
+    let items = [0, 1, 2, 3, 4, 5, 6];
+    let a = 2..4;
+    assert_eq!(a.start, 2);
+    assert_eq!(a.end, 4);
+    println!("{:?}", &items[a]);
 }
