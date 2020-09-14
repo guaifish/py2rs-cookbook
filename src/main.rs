b7fd3add2c09087fa113a3337475a95010d93eea
guaifish <guaifish@hotmail.com>
2020-09-14 15:03:12 +0800

    1.5 实现一个优先级队列

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,13 @@
-use std::collections::BinaryHeap;
-
-trait Largest<T> {
-    fn nlargest(&self, n: u32) -> Vec<T>;
-}
-
-impl<T: Ord + Copy> Largest<T> for BinaryHeap<T> {
-    fn nlargest(&self, n: u32) -> Vec<T> {
-        let mut heaq = self.clone();
-        let mut v: Vec<T> = Vec::new();
-        if n > self.len() as u32 {
-            panic!("n is bigger than heaq's lengh")
-        }
-        for _ in 0..n {
-            if let Some(x) = heaq.pop() {
-                v.push(x)
-            } else {
-                unimplemented!()
-            }
-        }
-        v
-    }
-}
+use py2rs_cookbook::PriorityQueue;
 
 fn main() {
-    let nums = vec![1, 8, 2, 23, 7, -4, 18, 23, 42, 37, 2];
-    // 最大堆
-    let heap = BinaryHeap::from(nums);
-    println!("{:?}", heap.nlargest(3));
+    let mut q = PriorityQueue::new();
+    q.push("foo", 1);
+    q.push("bar", 5);
+    q.push("spam", 4);
+    q.push("grok", 1);
+    assert_eq!(q.pop(), "bar");
+    assert_eq!(q.pop(), "spam");
+    assert_eq!(q.pop(), "foo");
+    assert_eq!(q.pop(), "grok");
 }
