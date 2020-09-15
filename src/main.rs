fd37541163840f257d925cd061f99d34288a39c7
guaifish <guaifish@hotmail.com>
2020-09-15 17:14:26 +0800

    2.20 字节字符串上的字符串操作

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use htmlescape::{decode_html, encode_minimal};
-
 fn main() {
-    let s1 = r#"Elements are written as "<tag>text</tag>"."#;
-    let s2 = "Elements are written as &quot;&lt;tag&gt;text&lt;/tag&gt;&quot;.";
-    assert_eq!(encode_minimal(s1), s2);
-    assert_eq!(s1, decode_html(s2).unwrap());
+    let data = b"Hello World";
+    assert_eq!(&data[0..5], b"Hello");
+    assert_eq!(String::from_utf8_lossy(&data[0..5]), "Hello");
+    let mut iter = data.split(|x| *x == b' ');
+    assert_eq!(iter.next().unwrap(), b"Hello");
+    assert_eq!(iter.next().unwrap(), b"World");
 }
