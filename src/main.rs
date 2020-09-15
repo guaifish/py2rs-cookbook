d260500b35560228f5693c34758820ba9c372943
guaifish <guaifish@hotmail.com>
2020-09-15 16:20:18 +0800

    2.16 以指定列宽格式化字符串

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
+use textwrap::{NoHyphenation, Wrapper};
+
 fn main() {
-    let s = format!("{name} has {n} messages.", name = "Guido", n = 37);
-    println!("{}", s);
+    let s = "Look into my eyes, look into my eyes, the eyes, the eyes, \
+            the eyes, not around the eyes, don't look around the eyes, \
+            look into my eyes, you're under.";
+
+    println!("{}", textwrap::fill(s, 70));
+    println!("\n");
+
+    let wrapper = Wrapper::with_splitter(40, NoHyphenation)
+        .initial_indent("    ")
+        .subsequent_indent("");
+    println!("{}", wrapper.fill(s));
 }
