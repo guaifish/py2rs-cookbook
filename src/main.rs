75856e224f9ddaf38755a9e919e00459b09afa0d
guaifish <guaifish@hotmail.com>
2020-09-15 11:42:06 +0800

    2.6 字符串忽略大小写的搜索替换

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
 use regex::Regex;
 
 fn main() {
-    let text = r"yeah, but no, but yeah, but no, but yeah";
-    assert_eq!(
-        text.replace("yeah", "yep"),
-        r"yep, but no, but yep, but no, but yep"
-    );
-
-    let text = r"Today is 11/27/2012. PyCon starts 3/13/2013.";
-    let re = Regex::new(r"(?P<month>\d+)/(?P<day>\d+)/(?P<year>\d+)").unwrap();
-    let result = re.replace_all(text, r"$year-$month-$day");
-    assert_eq!(result, r"Today is 2012-11-27. PyCon starts 2013-3-13.");
+    let text = r"UPPER PYTHON, lower python, Mixed Python";
+    let re = Regex::new(r"(?i)python").unwrap();
+    let result = re.replace_all(text, "snake");
+    assert_eq!(result, r"UPPER snake, lower snake, Mixed snake");
 }
