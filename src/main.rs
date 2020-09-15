eaf3f1be5658994f845355f953463e552b17b561
guaifish <guaifish@hotmail.com>
2020-09-15 11:24:00 +0800

    2.4 字符串匹配和搜索

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-fn main() {
-    let filename = "spam.txt";
-    assert_eq!(filename.ends_with(".txt"), true);
+use regex::Regex;
 
-    let url = "http://www.python.org";
-    assert_eq!(url.starts_with("http:"), true);
+fn main() {
+    let re = Regex::new(r"(\d+)/(\d+)/(\d+)").unwrap();
+    assert!(re.is_match(r"11/27/2012"));
+    let caps = re.captures(r"11/27/2012").unwrap();
+    assert_eq!(caps.get(1).unwrap().as_str(), "11");
+    assert_eq!(caps.get(2).unwrap().as_str(), "27");
+    assert_eq!(caps.get(3).unwrap().as_str(), "2012");
+    assert_eq!(caps.get(0).unwrap().as_str(), "11/27/2012");
 }
