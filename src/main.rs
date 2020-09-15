8dde7925ecf3971a4c9fc0277a3e69fb9096f90b
guaifish <guaifish@hotmail.com>
2020-09-15 15:34:52 +0800

    2.13 字符串对齐

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    let s = " hello world \n";
-    assert_eq!(s.trim(), "hello world");
-    assert_eq!(s.trim_start(), "hello world \n");
-    assert_eq!(s.trim_end(), " hello world");
-
-    let t = "-----hello=====";
-    assert_eq!(t.trim_start_matches('-'), "hello=====");
-    assert_eq!(t.trim_end_matches('='), "-----hello");
-    let pat: &[_] = &['-', '='];
-    assert_eq!(t.trim_matches(pat), "hello");
+    let text = "Hello World";
+    let x = 1.2345;
+    println!("{:<20}", text);
+    println!("{:>20}", text);
+    println!("{:^20}", text);
+    println!("{:=>20}", text);
+    println!("{:*^20}", text);
+    println!("{:>10}", x);
+    println!("{:.2}", x);
 }
