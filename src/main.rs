29b7add41feb20ee4a53c9f7b7de10bc79da520d
guaifish <guaifish@hotmail.com>
2020-09-16 00:53:33 +0800

    3.8 分数运算

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
+use num_rational::Ratio;
+
 fn main() {
-    assert_ne!(0.0_f64 / 0.0_f64, 0.0_f64 / 0.0_f64);
-    assert_ne!(f64::NAN, f64::NAN);
-    assert_eq!(1.0_f64 / 0.0_f64, f64::INFINITY);
-    assert_eq!(-1.0_f64 / 0.0_f64, f64::NEG_INFINITY);
-
-    assert_eq!(f64::INFINITY + 45.0, f64::INFINITY);
-    assert_eq!(f64::INFINITY * 10.0, f64::INFINITY);
-    assert_eq!(10.0 / f64::INFINITY, 0.0);
-
-    assert_eq!(format!("{:?}", f64::INFINITY / f64::INFINITY), "NaN");
-    assert_eq!(format!("{:?}", f64::INFINITY / f64::NEG_INFINITY), "NaN");
-    assert_eq!(format!("{:?}", f64::NAN + 23.0), "NaN");
-    assert_eq!(format!("{:?}", f64::NAN / 2.0), "NaN");
-    assert_eq!(format!("{:?}", f64::NAN * 2.0), "NaN");
-    assert_eq!(format!("{:?}", f64::NAN.sqrt()), "NaN");
+    let a = Ratio::new(5, 4);
+    let b = Ratio::new(7, 16);
+    let c = Ratio::new(35, 64);
+    assert_eq!(a + b, Ratio::new(27, 16));
+    assert_eq!(a * b, c);
+    assert_eq!(c.numer(), &35);
+    assert_eq!(c.denom(), &64);
 }
