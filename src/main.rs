e2df5ca312bbdd6b9e7ef1c2992f274440f8f31f
guaifish <guaifish@hotmail.com>
2020-09-15 14:50:13 +0800

    2.8 多行匹配模式

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new(r#""(.*?)""#).unwrap();
-    let text1 = r#"Computer says "no.""#;
-    let result = re.captures(text1).unwrap();
-    assert_eq!(result.get(1).unwrap().as_str(), "no.");
-
-    let text2 = r#"Computer says "no." Phone says "yes.""#;
-    let result: Vec<_> = re
-        .captures_iter(text2)
-        .map(|x| x.get(1).unwrap().as_str())
-        .collect();
-    assert_eq!(result, vec!["no.", "yes."]);
+    let re = Regex::new(r"(?s)/\*(.*?)\*/").unwrap();
+    let text2 = r#"/* this is a
+    multiline comment */
+    "#;
+    let result = re.captures(text2).unwrap();
+    assert_eq!(
+        result.get(1).unwrap().as_str(),
+        " this is a\n    multiline comment "
+    );
 }
