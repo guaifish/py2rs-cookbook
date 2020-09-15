6eb1796d65233d954f108e441054464a20b59d2b
guaifish <guaifish@hotmail.com>
2020-09-15 15:50:43 +0800

    2.15 字符串中插入变量

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let parts = ["Is", "Chicago", "Not", "Chicago?"];
-    assert_eq!(parts.join(" "), "Is Chicago Not Chicago?");
-    assert_eq!(parts.join(","), "Is,Chicago,Not,Chicago?");
-    assert_eq!(parts.join(""), "IsChicagoNotChicago?");
-
-    let a = "Is Chicago";
-    let b = "Not Chicago?";
-    assert_eq!(a.to_owned() + " " + b, "Is Chicago Not Chicago?");
+    let s = format!("{name} has {n} messages.", name = "Guido", n = 37);
+    println!("{}", s);
 }
