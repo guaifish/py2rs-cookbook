6233549b2d3467bba8831524ec007633bb244d4a
guaifish <guaifish@hotmail.com>
2020-09-16 21:18:54 +0800

    3.11 随机选择

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use ndarray::array;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 fn main() {
-    let a1 = array![1, 2, 3, 4];
-    let a2 = array![2, 4, 6, 8];
-    let a3 = array![11, 12, 13, 14];
-    let a4 = array![5, 6, 7, 8];
-    let a5 = array![5, 12, 21, 32];
-    assert_eq!(&a1 * 2, a2);
-    assert_eq!(&a1 + 10, a3);
-    assert_eq!(&a1 * &a4, a5);
+    // choose
+    let values = [1, 2, 3, 4, 5, 6];
+    let mut rng = thread_rng();
+    for _ in 0..5 {
+        println!("{:?}", values.choose(&mut rng).unwrap());
+    }
 
-    let a = array![[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+    // random
+    let x: u8 = rand::random();
+    println!("x = {}", x);
+
+    let a: [u8; 3] = rand::random();
+    println!("a = {:?}", a);
+
+    // shuffle
+    let mut values = [1, 2, 3, 4, 5, 6];
+    values.shuffle(&mut rng);
+    println!("{:?}", values);
 }
