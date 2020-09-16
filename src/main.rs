47dc0dbaeb240629556ff07394008be9673ab51e
guaifish <guaifish@hotmail.com>
2020-09-16 22:40:04 +0800

    3.12 基本的日期与时间转换

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use chrono::prelude::*;
 
 fn main() {
-    // choose
-    let values = [1, 2, 3, 4, 5, 6];
-    let mut rng = thread_rng();
-    for _ in 0..5 {
-        println!("{:?}", values.choose(&mut rng).unwrap());
-    }
+    let utc = Utc::now();
+    let local = Local::now();
+    println!("{:?}", utc);
+    println!("{:?}", local);
+    let today = Local::today();
+    println!("{:?}", today.format("%Y-%m-%d").to_string());
 
-    // random
-    let x: u8 = rand::random();
-    println!("x = {}", x);
+    let dt = Utc.ymd(2014, 11, 28).and_hms(12, 0, 9);
+    assert_eq!(
+        dt.format("%Y-%m-%d %H:%M:%S").to_string(),
+        "2014-11-28 12:00:09"
+    );
+    assert_eq!(dt.to_string(), "2014-11-28 12:00:09 UTC");
 
-    let a: [u8; 3] = rand::random();
-    println!("a = {:?}", a);
+    let dt = Utc.ymd(2014, 11, 28).and_hms(12, 0, 9);
+    let fixed_dt = dt.with_timezone(&FixedOffset::east(9 * 3600));
 
-    // shuffle
-    let mut values = [1, 2, 3, 4, 5, 6];
-    values.shuffle(&mut rng);
-    println!("{:?}", values);
+    assert_eq!(
+        "2014-11-28T12:00:09Z".parse::<DateTime<Utc>>(),
+        Ok(dt.clone())
+    );
+    assert_eq!(
+        "2014-11-28T21:00:09+09:00".parse::<DateTime<Utc>>(),
+        Ok(dt.clone())
+    );
+    assert_eq!(
+        "2014-11-28T21:00:09+09:00".parse::<DateTime<FixedOffset>>(),
+        Ok(fixed_dt.clone())
+    );
 }
