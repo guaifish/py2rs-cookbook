8cf581612c085655980ded01f8cbc010ac785549
guaifish <guaifish@hotmail.com>
2020-09-16 20:53:19 +0800

    3.9 大型数组运算

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use num_rational::Ratio;
+use ndarray::array;
 
 fn main() {
-    let a = Ratio::new(5, 4);
-    let b = Ratio::new(7, 16);
-    let c = Ratio::new(35, 64);
-    assert_eq!(a + b, Ratio::new(27, 16));
-    assert_eq!(a * b, c);
-    assert_eq!(c.numer(), &35);
-    assert_eq!(c.denom(), &64);
+    let a1 = array![1, 2, 3, 4];
+    let a2 = array![2, 4, 6, 8];
+    let a3 = array![11, 12, 13, 14];
+    let a4 = array![5, 6, 7, 8];
+    let a5 = array![5, 12, 21, 32];
+    assert_eq!(&a1 * 2, a2);
+    assert_eq!(&a1 + 10, a3);
+    assert_eq!(&a1 * &a4, a5);
+
+    let a = array![[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
 }
